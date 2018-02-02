f531ce6a56199a125c9ac0a78dd6d726acd96bdb
Leonardo Banderali <leonardo2718@protonmail.com>
2018-02-02 12:14:35 -0700

    Further improve praser

--- src/MRDParser.hs
+++ src/MRDParser.hs
@@ -106,12 +106,21 @@ factor -> LPAR expr RPAR
 
 -}
 
+import Data.List
+
 import CompilerEnvironment
 import MLexer
 
 parseError :: String -> Token -> CompilerMonad a
 parseError mkmsg t@(Token _ pos@(AlexPn _ l c)) = logError msg where
     msg = concat $ ["Parsing error at ", showAlexPos pos, ": ", mkmsg]
+
+showFirst :: Show a => Int -> [a] -> String
+showFirst n l = if length l > n
+    then concat ["[", intercalate ", " (map show firsts), ", ..."]
+    else show firsts
+    where
+        firsts = take n l
 
 data AST = AST Statement
 data Statement = IfThenElse {stmtExpr :: Expression, thenBranch :: Statement, elseBranch :: Statement}
@@ -130,7 +139,7 @@ data Expression = Add Expression Expression
 logTerminal :: Token -> [Token] -> CompilerMonad ()
 logTerminal t ts = do
     logMsgLn ("-- found terminal: " ++ show t)
-    -- logMsgLn ("-- remaining tokens: " ++ show ts)
+    logMsgLn ("   remaining tokens: " ++ showFirst 4 ts)
 
 eatTerminal :: TokenType -> [Token] -> CompilerMonad [Token]
 eatTerminal tt ts = case ts of
@@ -138,8 +147,9 @@ eatTerminal tt ts = case ts of
     t@(Token tt' _):ts' -> if tt == tt'
         then do
             logMsgLn ("-- eating terminal: " ++ show t)
+            logMsgLn ("   remaining tokens: " ++ showFirst 4 ts')
             return ts'
-        else parseError (concat ["Unexpected token\n    expecting ", show tt, " but found ", show tt', " instead"]) t
+        else parseError (concat ["Missing ", show tt, " token"]) t
 
 parse :: [Token] -> CompilerMonad [Token]
 parse ts = do
@@ -151,32 +161,30 @@ parseStatement ts = do
     logMsgLn "Looking for a Statement"
     stmt <- case ts of
         t@(Token IF _):ts' -> do
-            logMsgLn "-- parsing If Then Else statement"
             logTerminal t ts'
+            logMsgLn "-- parsing If Then Else statement"
             parseExpression ts' >>= eatTerminal THEN >>= parseStatement >>= eatTerminal ELSE >>= parseStatement
         t@(Token WHILE _):ts' -> do
-            logMsgLn "-- parsing While Do statement"
             logTerminal t ts'
+            logMsgLn "-- parsing While Do statement"
             parseExpression ts' >>= eatTerminal DO >>= parseStatement
         t1@(Token INPUT _):t2@(Token (ID _) _):ts' -> do
-            logMsgLn "-- parsing Input statement"
             logTerminal t1 (t2:ts')
             logTerminal t2 ts'
+            logMsgLn "-- parsing Input statement"
             return ts'
         t1@(Token (ID _) _):t2@(Token ASSIGN _):ts' -> do
-            logMsgLn "-- parsing Assignment"
             logTerminal t1 (t2:ts')
             logTerminal t2 ts'
-            logMsgLn "Looking for an expression"
+            logMsgLn "-- parsing Assignment"
             parseExpression ts'
         t@(Token WRITE _):ts' -> do
-            logMsgLn "-- parsing Write statement"
             logTerminal t ts'
-            logMsgLn "Looking for an expression"
+            logMsgLn "-- parsing Write statement"
             parseExpression ts'
         t@(Token BEGIN _):ts' -> do
-            logMsgLn "-- parsing Block statement"
             logTerminal t ts'
+            logMsgLn "-- parsing Block statement"
             parseStatementList ts'
         [] -> compError "Expecting more tokens to parse a Statement"
         t@(Token tt _):_ -> parseError ("Unexpected token: " ++ show tt) t
@@ -196,21 +204,30 @@ parseStatementList ts = logMsgLn "Looking for Sub-Statements" >> case ts of
 parseExpression :: [Token] -> CompilerMonad [Token]
 parseExpression [] = compError "Expecting more tokens to parse expression"
 parseExpression ts = do
-    logMsgLn "-- looking for a Term"
-    parseTerm ts >>= eatAddOp
+    logMsgLn "Looking for an Expression"
+    parseSubExpression ts
     where
-        parseTerm ts = logMsgLn "-- looking for a factor" >> parseFactor ts >>= eatMulOp
-        parseFactor [] = compError "Expecting more tokens to parse expression"
-        parseFactor (t1@(Token SUB _):t2@(Token (NUM _) _):ts') = do
-            logMsg "-- found factor"
-            logTerminal t1 (t2:ts)
-            logTerminal t2 ts
+        parseSubExpression :: [Token] -> CompilerMonad [Token]
+        parseSubExpression ts = do
+            logMsgLn "-- looking for a Subexpression"
+            parseTerm ts >>= eatAddOp
+        parseTerm ts = do
+            logMsgLn "-- looking for a Term"
+            parseFactor ts >>= eatMulOp
+        parseFactor ts = do
+            logMsgLn "-- looking for a factor"
+            ts' <- case ts of
+                t1@(Token SUB _):t2@(Token (NUM _)_):ts' -> do
+                    logTerminal t1 (t2:ts')
+                    logTerminal t2 ts'
+                    return ts'
+                t@(Token LPAR _):ts' -> parseExpression ts' >>= eatTerminal RPAR
+                t@(Token (ID _) _):ts' -> return ts'
+                t@(Token (NUM _) _):ts' -> return ts'
+                t@(Token tt _):ts'-> parseError ("Unexpected token: " ++ show tt) t
+                [] -> logError "Expecting more tokens to parse expression"
+            logMsgLn "-- found factor"
             return ts'
-        parseFactor (t:ts') = logMsgLn "-- found factor" >> logTerminal t ts' >> case t of
-            Token LPAR _    -> logMsgLn "-- looking for subexpression" >> parseExpression ts' >>= eatTerminal RPAR
-            Token (ID _) _  -> return ts'
-            Token (NUM _) _ -> return ts'
-            Token tt _      -> parseError ("Unexpected token: " ++ show tt) t
         eatAddOp (t:ts') = logTerminal t ts' >> case t of
             Token ADD _ -> parseExpression ts'
             Token SUB _ -> parseExpression ts'
